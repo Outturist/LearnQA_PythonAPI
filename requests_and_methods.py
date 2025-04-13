0a022a521c40fc549eb51fc2ae84138102c6b1c1
Outturist <0ecpjuzphnun@mail.ru>
2025-04-13 21:24:38 +0300

    added requests_and_methods.py

--- requests_and_methods.py
+++ requests_and_methods.py
@@ -1,25 +1,27 @@
 import requests
 
 response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type")
-print(response.text)
+print('кейс_1 текст:', response.text)
 
 response_2 = requests.head("https://playground.learnqa.ru/ajax/api/compare_query_type", data={"method": "HEAD"})
-print(response_2.text)
-print(response_2.status_code)
+print('кейс_2 текст:', response_2.text)
+print('кейс_2 код:', response_2.status_code)
 
 response_3 = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", data={"method": "DELETE"})
-print(response_3.text)
+print('кейс_3 текст:', response_3.text)
 
 
 methods = ['get', 'post', 'put', 'delete']
 
 for method in methods:
     if method == 'get':
-        response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type",
-                                     params={"method": "GET"})
-        print(response.text)
+        values = ["GET", "POST", "PUT", "DELETE"]
+        for value in values:
+            response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type",
+                                         params={"method": f"{value}"})
+            print(f"Метод: {method}, Параметр: {value} Ответ: {response.text}")
     else:
-        values = ["POST", "PUT", "DELETE"]
+        values = ["GET", "POST", "PUT", "DELETE"]
         for value in values:
             response = requests.request(method, "https://playground.learnqa.ru/ajax/api/compare_query_type",
                                     data={"method": f"{value}"})
